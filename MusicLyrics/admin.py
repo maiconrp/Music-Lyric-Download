710daedd36db2cb5a5e0a235d5cebbbf0acee3cf
Maicon Robert <maiconlk2321@gmail.com>
2022-09-17 17:00:07 -0300

    :construction: feat: Registrando models

--- MusicLyrics/admin.py
+++ MusicLyrics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Pesquisa)
+admin.site.register(Artista)
+admin.site.register(Musica)
+admin.site.register(Letra)
