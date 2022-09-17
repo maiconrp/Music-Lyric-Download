8f3ec5b255773546e3b914ac12f1b0d9e6db5aa8
Maicon Robert <maiconlk2321@gmail.com>
2022-09-17 16:56:17 -0300

    :construction: build: Install 'multiselectfield'

--- MusicLyricProject/settings.py
+++ MusicLyricProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MusicLyrics.apps.MusiclyricsConfig'
+    'MusicLyrics.apps.MusiclyricsConfig',
+    'multiselectfield'
 ]
 
 MIDDLEWARE = [
