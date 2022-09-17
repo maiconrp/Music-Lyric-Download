4be29a9a289c7113a85a3393332980b4cf42a986
Maicon Robert <maiconlk2321@gmail.com>
2022-09-17 17:14:37 -0300

    :wrench: feat: Configurando render template

--- MusicLyrics/views.py
+++ MusicLyrics/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from Scripts import PesquisarMusica
 
 def index(request):
-    return HttpResponse("Index")
+     return render(request, 'home.html', {})
+
+def pesquisar(request):
+
+    return render(request, 'home.html', {})
 # Create your views here
 
