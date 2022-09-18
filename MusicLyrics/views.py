1b9c2e1f7705f505262da2ea23bf892c9aafe200
Maicon Robert <maiconlk2321@gmail.com>
2022-09-18 01:24:59 -0300

    🚧 config: view de pesquisa

--- MusicLyrics/views.py
+++ MusicLyrics/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from Scripts import PesquisarMusica
+from MusicLyrics.forms import * 
+from MusicLyrics.models import * 
 
 def index(request):
      return render(request, 'home.html', {})
 
 def pesquisar(request):
+    if request.method == 'POST':
+        pesquisa = PesquisaForm(request.POST)
 
-    return render(request, 'home.html', {})
+        if pesquisa.is_valid():
+            pesquisa.save()
+
+            context = {
+                'form'   : pesquisa,
+            }
+            return render(request, 'home.html', context)
+    
+    else:
+        pesquisa = PesquisaForm()
+
+    context = {
+        'form' : pesquisa,
+        
+    }
+    
+    return render(request, 'home.html', context)
 # Create your views here
-
